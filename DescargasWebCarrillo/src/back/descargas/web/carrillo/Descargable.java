00a2fd989972a1f39ca6542f22a42a6fb2b6c982
agarca4 <carrillofuente@hotmail.com>
2019-12-18 09:05:39 +0100

    modificando el compareTo

--- DescargasWebCarrillo/src/back/descargas/web/carrillo/Descargable.java
+++ DescargasWebCarrillo/src/back/descargas/web/carrillo/Descargable.java
@@ -8,16 +8,8 @@ public interface Descargable extends Comparable<Descargable> {
 
 	@Override
 	public default int compareTo(Descargable arg0) {
-		int resultado;
-		if (this.getContenido().getId().getIdentificador() < arg0.getContenido().getId().getIdentificador()) {
-			resultado = -1;
-		} else if (this.getContenido().getId().getIdentificador() > arg0.getContenido().getId().getIdentificador()) {
-			resultado = 1;
-		} else {
-			resultado = 0;
-		}
 
-		return resultado;
+		return this.getContenido().getId().getIdentificador() - arg0.getContenido().getId().getIdentificador();
 
 	}
 
